d9d442bb7289e5a8d988a19cdc18a59e5211ad37
nibbler13 einsamkeit <nibble@yandex.ru>
2018-05-31 10:48:38 +0300

    Добавьте файлы проекта.

---
+++ Infoscreen/DataHandle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infoscreen {
+	class DataHandle {
+		private FirebirdClient firebirdClient;
+
+		public DataHandle() {
+			firebirdClient = new FirebirdClient(
+				Properties.Settings.Default.MisDbAddress,
+				Properties.Settings.Default.MisDbName,
+				Properties.Settings.Default.MisDbUser,
+				Properties.Settings.Default.MisDbPassword);
+		}
+	}
+}
